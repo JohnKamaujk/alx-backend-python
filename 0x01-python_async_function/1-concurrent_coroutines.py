609ecb40afc065da20745e70624bc31757fb02b7
JohnKamaujk <johnjkkamau@gmail.com>
2024-01-15 18:29:03 +0300

    :white_check_mark:Complete:Task 2 complete

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -4,7 +4,7 @@
 import asyncio
 from typing import List
 
-wait_random = __import__('0-basic_async_syntax.py').wait_random
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
@@ -15,7 +15,7 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     :param max_delay: Maximum delay in seconds.
     :return: List of delays in ascending order.
     """
-    wait_times = await asyncio.gather(
-        *tuple(map(lambda _: wait_random(max_delay), range(n)))
-    )
-    return sorted(wait_times)
+    tasks = [wait_random(max_delay) for _ in range(n)]
+    results = await asyncio.gather(*tasks)
+    sorted_results = sorted(results)
+    return sorted_results
