10e682caa3c22077ab4e32545ff554101a29a47a
JohnKamaujk <johnjkkamau@gmail.com>
2024-02-02 15:45:37 +0300

    :white_check_mark:Pass exception test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -19,3 +19,14 @@ class TestAccessNestedMap(unittest.TestCase):
                                expected: Union[Dict, int]) -> None:
         """Tests `access_nested_map`'s output."""
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",), KeyError),
+        ({"a": 1}, ("a", "b"), KeyError),
+    ])
+    def test_access_nested_map_exception(self, nested_map: Dict,
+                                         path: Tuple[str],
+                                         exception: Exception) -> None:
+        """Tests `access_nested_map`'s Exception output."""
+        with self.assertRaises(exception):
+            access_nested_map(nested_map, path)
