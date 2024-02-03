992d14fb3c2390e2c5213c31d1dffe42bc4f02f5
JohnKamaujk <johnjkkamau@gmail.com>
2024-02-03 17:58:38 +0300

    :white_check_mark:memoize tests

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,7 +4,7 @@
 import unittest
 from unittest.mock import patch, Mock
 from parameterized import parameterized
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 from typing import Dict, Tuple, Union
 
 
@@ -39,9 +39,32 @@ class TestGetJson(unittest.TestCase):
         ("http://example.com", {"payload": True}),
         ("http://holberton.io", {"payload": False}),
     ])
-    def test_get_json(self, test_url: str, test_payload: Dict):
+    def test_get_json(self, test_url: str, test_payload: Dict) -> None:
         """Tests `get_json`'s output."""
         attr = {'json.return_value': test_payload}
         with patch("requests.get", return_value=Mock(**attr)) as req_get:
             self.assertEqual(get_json(test_url), test_payload)
             req_get.assert_called_once_with(test_url)
+
+
+class TestMemoize(unittest.TestCase):
+    """Tests the `memoize` function."""
+    def test_memoize(self) -> None:
+        """Tests `memoize` output."""
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass,
+                          "a_method",
+                          return_value=lambda: 42) as mock_method:
+            test_instance = TestClass()
+            result1 = test_instance.a_property()
+            result2 = test_instance.a_property()
+            mock_method.assert_called_once()
+            self.assertEqual(result1, 42)
+            self.assertEqual(result2, 42)
